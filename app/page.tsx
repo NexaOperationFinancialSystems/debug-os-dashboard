3448256bac1c4e0b8bf77cfe0011982a3407b91b
NEXA Operating Financial Systems <nexaofs@gmail.com>
2026-03-22 17:24:50 -0400

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>🔥 Debug OS Dashboard Home</h1>
-      <p>Main system is now connected to GitHub.</p>
-    </div>
-  );
-}
