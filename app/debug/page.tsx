da9c406e4444c42fef138427115c4f19370dfc4d
NEXA Operating Financial Systems <nexaofs@gmail.com>
2026-03-22 18:44:17 -0400

    Delete app/debug/page.tsx

--- app/debug/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DebugPage() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>🚀 Debug OS Dashboard</h1>
-      <p>System monitoring is ACTIVE.</p>
-    </div>
-  );
-}
